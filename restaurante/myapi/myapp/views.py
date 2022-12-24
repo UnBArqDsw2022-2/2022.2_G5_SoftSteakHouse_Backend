eed788061e64abf3cb8f98e03a735a9b7775878c
oCaioOliveira <caiocesar31951910@hotmail.com>
2022-12-24 15:15:31 -0300

    :floppy_disk: Adição da opção de busca por item e adicional

--- restaurante/myapi/myapp/views.py
+++ restaurante/myapi/myapp/views.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
-from .models import Item
-from .serializers import ItemSerializer
+from rest_framework.filters import SearchFilter
+from .models import Item, Adicional
+from .serializers import ItemSerializer, AdicionalSerializer
 
 class ItemViewSet(viewsets.ModelViewSet):
 
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['titulo']
+
+class AdicionalViewSet(viewsets.ModelViewSet):
+
+    queryset = Adicional.objects.all()
+    serializer_class = AdicionalSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['nome']
